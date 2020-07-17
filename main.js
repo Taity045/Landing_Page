c62c3550f51289bb413a9f71aa0cf443bc8e7f50
Tatenda <tatenda@Dell>
2020-07-17 22:16:50 +0200

    changes here and there

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-// <!--js for the moving text effect-->
+// // <!--js for the moving text effect-->
    
-    // <script>
-    //   $(function(){
-    //   $(".nouns").typed({
-    //     strings: ["travel", "trees", "sunshine"],
-    //   typeSpeed: 0,
-    //   showCursor: false,
-    //   loop: true,
-    //   loopCount: false,
-    //   backDelay: 1000,
-    //   })
-    // </script>
-   
-
+//     // <script>
+//     //   $(function(){
+//     //   $(".nouns").typed({
+//     //     strings: ["travel", "trees", "sunshine"],
+//     //   typeSpeed: 0,
+//     //   showCursor: false,
+//     //   loop: true,
+//     //   loopCount: false,
+//     //   backDelay: 1000,
+//     //   })
+//     // </script>
    
 
-    fetch('https://ocpi5trgye.execute-api.us-east-1.amazonaws.com/Prod/index//')
-    .then((res) => res.json())
-    .then((data) => {
-    loginBarner.innerHTML = `${JSON.stringify(data)}`;
-    console.log(data);
-    })
    
+
+//Visitors counter
+
+fetch('https://ocpi5trgye.execute-api.us-east-1.amazonaws.com/Prod/index/')
+  .then(res => res.json())
+  .then(count => {
+    document.querySelector('.visitor-count').textContent = count.numberofVisitors?count.numberofVisitors:400;
+  })
