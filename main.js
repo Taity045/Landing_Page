1a10cd60eb9b0e28d6225b53309f3ac15041789c
Taity045 <tatendagibsonm@gmail.com>
2020-09-17 13:13:53 +0200

    updates

--- main.js
+++ main.js
@@ -17,8 +17,43 @@
 
 //Visitors counter
 
-fetch('https://ocpi5trgye.execute-api.us-east-1.amazonaws.com/Prod/index/')
-  .then(res => res.json())
-  .then(count => {
-    document.querySelector('.visitor-count').textContent = count.numberofVisitors?count.numberofVisitors:400;
+ fetch('https://ocpi5trgye.execute-api.us-east-1.amazonaws.com/Prod/index/')
+   .then(res => res.json())
+   .then(count => {
+     document.querySelector('.visitor-count').textContent = count.numberofVisitors?count.numberofVisitors:400;
   })
+
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+
+$(document).ready(function(){
+  // Now to start autoTyping just call the autoType function with the 
+  // class of outer div
+  // The second paramter is the speed between each letter is typed.   
+  autoType(".type-js",200);
+});
